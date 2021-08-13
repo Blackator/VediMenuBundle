59a903fb2394382136cb79303ca6e0bf3b2e71d5
Dmitry Titov <blackator@yandex.ru>
2021-08-13 13:10:53 +0300

    Minor fix.

--- src/Loaders/AbstractMenuLoader.php
+++ src/Loaders/AbstractMenuLoader.php
@@ -5,7 +5,6 @@ namespace Blackator\Bundle\VediMenuBundle\Loaders;
 use Blackator\Bundle\VediMenuBundle\Component\Menu;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Component\Security\Core\Security;
-use Symfony\Contracts\Translation\TranslatorInterface;
 use Twig\Environment;
 
 /**
@@ -22,18 +21,16 @@ abstract class AbstractMenuLoader
     /**
      * Connecting services required for menu generation.
      * @param UrlGeneratorInterface $urlGenerator URL generator
-     * @param TranslatorInterface $translator Translator
      * @param Environment $twig TWIG
      * @param Security $security Security
      * @return $this
      */
     public function setServices(UrlGeneratorInterface $urlGenerator, Environment $twig, Security $security, $translator = null): self
     {
-        $this->setUrlGenerator($urlGenerator);
-        $this->setTwig($twig);
-        $this->setSecurity($security);
-        $this->translator = $translator;
-        if ($translator === null || in_array('Symfony\Contracts\Translation\TranslatorInterface', class_implements($translator))) $this->translator = $translator;
+        $this->urlGenerator = $urlGenerator;
+        $this->twig = $twig;
+        $this->security = $security;
+        if ($translator === null || in_array('Symfony\Contracts\Translation\TranslatorInterface', class_implements($translator, false))) $this->translator = $translator;
         return $this;
     }
 
@@ -41,66 +38,6 @@ abstract class AbstractMenuLoader
     {
         if ($this->translator !== null) return $this->translator->trans($source, $params, $domain);
         return $source;
-    }
-
-    /**
-     * Get URL generator
-     * @return UrlGeneratorInterface|null
-     */
-    public function getUrlGenerator(): ?UrlGeneratorInterface
-    {
-        return $this->urlGenerator;
-    }
-
-    /**
-     * Set URL generator
-     * @param UrlGeneratorInterface $urlGenerator URL generator service
-     * @return $this
-     */
-    public function setUrlGenerator(UrlGeneratorInterface $urlGenerator): self
-    {
-        $this->urlGenerator = $urlGenerator;
-        return $this;
-    }
-
-    /**
-     * Get TWIG
-     * @return Environment|null
-     */
-    public function getTwig(): ?Environment
-    {
-        return $this->twig;
-    }
-
-    /**
-     * Set TWIG
-     * @param Environment $twig TWIG Environment
-     * @return $this
-     */
-    public function setTwig(Environment $twig): self
-    {
-        $this->twig = $twig;
-        return $this;
-    }
-
-    /**
-     * Get translator
-     * @return TranslatorInterface|null
-     */
-    public function getTranslator(): ?TranslatorInterface
-    {
-        return $this->translator;
-    }
-
-    /**
-     * Set translator
-     * @param TranslatorInterface $translator Translator service
-     * @return $this
-     */
-    public function setTranslator(TranslatorInterface $translator): self
-    {
-        $this->translator = $translator;
-        return $this;
     }
 
     /**
